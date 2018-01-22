5ecbd37fbc54224f208c692a6de06b4f051a453e
Hannah Lee and Kayla Ondracek <kayla@>
2018-01-22 14:35:43 -0800

    add function to remove item from array and put it into completed items array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 var itemArray = [];
 var completedArray = [];
 var listItem = "";
+var i = -1;
+
+//Todo constructor
 function Todo(item, completion) {
   this.itemText = item;
   this.completedBool = completion;
@@ -11,9 +14,30 @@ function createList(item, completion) {
   itemArray.push(listItem);
   return itemArray;
 }
+//to remove item
+function removeItem(doneItemText) {
+  var index = 0;
+  itemArray.forEach(function(item) {
+    i++;
+    if (item.itemText === doneItemText) {
+      return index = i;
+    }
+  });
 
-function status(listItem) {
-  console.log(listItem);
-  // if (completedBool === true);
-  //   itemArray.indexOf(itemText)
+  var removedItem = itemArray.splice(index, 1);
+  completedArray.push(removedItem); 
 }
+
+
+$(document).ready(function() {
+  $("#todo-list").submit(function(event) {
+    event.preventDefault();
+    var inputItem = $("input#todo-item").val();
+
+    $("ul").append("<li><input type='checkbox' name='todo' value='" + inputItem + "'>" + inputItem + "</li>");
+
+
+    console.log($("input:checkbox[name=todo]:checked").val());
+
+  });
+});
