421beb7d532d302c08d237083b9a2cf1cb8faa12
Hannah Lee and Kayla Ondracek <hannah@>
2018-01-22 12:01:05 -0800

    create empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 var itemArray = [];
-
+var completedArray = [];
+var listItem = "";
 function Todo(item, completion) {
   this.itemText = item;
   this.completedBool = completion;
 }
 
 function createList(item, completion) {
-  var listItem = new Todo(item, completion);
+  listItem = new Todo(item, completion);
   itemArray.push(listItem);
   return itemArray;
+}
+
+function status(listItem) {
+  console.log(listItem);
+  // if (completedBool === true);
+  //   itemArray.indexOf(itemText)
 }
